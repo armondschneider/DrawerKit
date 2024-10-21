8c90b90c965a1f5203b89c55f931c853c31a9883
Armond Schneider <mondoloves@icloud.com>
2024-10-21 11:21:15 -0700

    Initial Commit

---
+++ DrawerKit/DrawerKit.h
@@ -0,0 +1,18 @@
+//
+//  DrawerKit.h
+//  DrawerKit
+//
+//  Created by Armond Schneider on 10/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DrawerKit.
+FOUNDATION_EXPORT double DrawerKitVersionNumber;
+
+//! Project version string for DrawerKit.
+FOUNDATION_EXPORT const unsigned char DrawerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerKit/PublicHeader.h>
+
+
---
+++ DrawerKitTests/DrawerKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerKitTests.swift
+//  DrawerKitTests
+//
+//  Created by Armond Schneider on 10/21/24.
+//
+
+import Testing
+@testable import DrawerKit
+
+struct DrawerKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
